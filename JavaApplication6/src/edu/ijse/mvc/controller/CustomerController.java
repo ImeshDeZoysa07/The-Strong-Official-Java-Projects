e6f1e54f3b6fd77366d0939e49ce27d3715dd178
Ranmuni Imesha Udayanga De Zoysa <155706447+ImeshDeZoysa07@users.noreply.github.com>
2024-01-11 12:37:08 +0530

    Add files via upload

---
+++ JavaApplication6/src/edu/ijse/mvc/controller/CustomerController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.controller;
+
+import edu.ijse.mvc.dto.CustomerDto;
+import edu.ijse.mvc.model.CustomerModel;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ImeshDeZoysa07
+ */
+public class CustomerController {
+    
+    private final CustomerModel CUSTOMER_MODEL;
+
+    public CustomerController() throws Exception {
+        this.CUSTOMER_MODEL = new CustomerModel();
+    }
+    
+    public String saveCustomer(CustomerDto customerDto) throws SQLException{
+        return CUSTOMER_MODEL.saveCustomer(customerDto);
+    }
+    
+    public ArrayList<CustomerDto> getAllCustomer() throws Exception{
+        return CUSTOMER_MODEL.getAllCustomer();
+    }
+    
+    public CustomerDto searchCustomer(String custId) throws Exception{
+        return CUSTOMER_MODEL.searchCustomer(custId);
+    }
+   
+       public String updateCustomer(CustomerDto customerDto) throws Exception {
+        return CUSTOMER_MODEL.updateCustomer(customerDto);
+    }
+       
+       public String deleteCustomer(String id) throws Exception {
+           return CUSTOMER_MODEL.deleteCustomer(id);
+    }
+}
